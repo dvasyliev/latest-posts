b7f372791d9a25199a69d190e5cfe38c98f3dc6b
dvasyliev <dvasyliev.dev@gmail.com>
2017-08-10 02:20:08 +0300

    Refactored FacebookUserAuthenihicated Middleware

--- app/Http/Middleware/FacebookUserAuthenticated.php
+++ app/Http/Middleware/FacebookUserAuthenticated.php
@@ -7,6 +7,13 @@ use Facebook\Facebook;
 
 class FacebookUserAuthenticated
 {
+    protected $fb;
+
+    public function __construct(Facebook $fb)
+    {
+        $this->fb = $fb;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -16,7 +23,40 @@ class FacebookUserAuthenticated
      */
     public function handle($request, Closure $next)
     {
-        echo 'run Middleware<br>';
+        $helper = $this->fb->getRedirectLoginHelper();
+
+        // Set state for Persistent Data Helper
+        if ($request->has('state')) {
+            $helper->getPersistentDataHandler()->set('state', $request->get('state'));
+        }
+
+        // Set Access Token
+        $accessToken = $request->session()->exists('facebook_access_token')
+            ? $request->session()->get('facebook_access_token')
+            : $helper->getAccessToken();
+
+        // Redirect to login page
+        if(!isset($accessToken)):
+            return redirect()->route('login');
+        endif;
+
+        // Generate Default Access Token
+        if(!$request->session()->exists('facebook_access_token')):
+            $oAuth2Client = $this->fb->getOAuth2Client();
+            $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken((string)$accessToken);
+            $request->session()->push('facebook_access_token', (string)$longLivedAccessToken);
+        endif;
+
+        // Set Default Access Token
+        $this->fb->setDefaultAccessToken(
+            $request->session()->get('facebook_access_token')
+        );
+
+        // Redirect to back
+        if($request->has('code')):
+            redirect()->route('home');
+        endif;
+
         return $next($request);
     }
 }
