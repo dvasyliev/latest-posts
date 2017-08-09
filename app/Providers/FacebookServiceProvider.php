2f116264e04bb29b08fa79716b7ca0f6be9f1202
dvasyliev <dvasyliev.dev@gmail.com>
2017-08-09 09:40:07 +0300

    Created FacebookServiceProvider and registered it. Created new instance of Facebook class with configurations

---
+++ app/Providers/FacebookServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Facebook\Facebook;
+use League\Flysystem\Config;
+
+class FacebookServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Facebook::class, function($app) {
+            return new Facebook(array(
+                'app_id' => Config::get('FACEBOOK_APP_ID'),
+                'app_secret' => Config::get('FACEBOOK_APP_SECRET'),
+            ));
+        });
+    }
+}
